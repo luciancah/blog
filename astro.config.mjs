d8dbff3b56f3eeea522514c96a6b31713e9bca1f
Jinhyung Lee <luciancah@gmail.com>
2024-07-10 17:05:39 +0900

    fix: 예전 버전으로 달아봄

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,10 @@ import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
 import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
   site: "https://astro-micro.vercel.app",
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
