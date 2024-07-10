c365ba5257fd532b3069a85f2c608c448f3d4c05
Jinhyung Lee <luciancah@gmail.com>
2024-07-10 17:01:46 +0900

    fix: vercel analytics on config mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,10 @@ import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
   site: "https://astro-micro.vercel.app",
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
@@ -15,8 +19,4 @@ export default defineConfig({
       theme: "css-variables",
     },
   },
-  output: "server",
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
 });
